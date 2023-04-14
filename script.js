3ebb0fa7cca0991c0463a7d509b8ad968b08ebcd
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2023-04-14 22:37:21 +0900

    Refactor

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
+// This function extracts the video ID from a YouTube URL
 function extractVideoID(videoURL) {
-  var videoID = "";
-  if (videoURL.indexOf("youtu.be/") != -1) { // If the URL is in the short format
-    videoID = videoURL.split("youtu.be/")[1];
-  } else if (videoURL.indexOf("v=") != -1) { // If the URL is in the long format
-    videoID = videoURL.split("v=")[1];
-    var ampersandPosition = videoID.indexOf("&");
-    if (ampersandPosition != -1) {
+  let videoID = '';
+  if (videoURL.indexOf('youtu.be/') !== -1) { // If the URL is in the short format
+    videoID = videoURL.split('youtu.be/')[1];
+  } else if (videoURL.indexOf('v=') !== -1) { // If the URL is in the long format
+    videoID = videoURL.split('v=')[1];
+    const ampersandPosition = videoID.indexOf('&');
+    if (ampersandPosition !== -1) {
       videoID = videoID.substring(0, ampersandPosition);
     }
   }
   return videoID;
 }
 
+// This function embeds the video with the given ID
 function embedVideo() {
-  var videoURL = document.getElementById("videoURL").value;
-  var videoID = extractVideoID(videoURL);
-  var videoEmbedURL = "https://www.youtube.com/embed/" + videoID;
-  var videoContainer = document.getElementById("videoContainer");
-  videoContainer.innerHTML = '<iframe width="854" height="480" src="' + videoEmbedURL + '" frameborder="0" allowfullscreen></iframe>';
+  const videoURL = document.getElementById('videoURL').value;
+  const videoID = extractVideoID(videoURL);
+  const videoEmbedURL = `https://www.youtube.com/embed/${videoID}`;
+  const videoContainer = document.getElementById('videoContainer');
+  videoContainer.innerHTML = `<iframe width="854" height="480" src="${videoEmbedURL}" frameborder="0" allowfullscreen></iframe>`;
   localStorage.setItem('videoID', videoID);
 }
 
+// This function loads the video with the given ID if it has been previously embedded
 window.onload = function () {
-  var videoID = localStorage.getItem('videoID');
+  const videoID = localStorage.getItem('videoID');
   if (videoID) {
-    var videoEmbedURL = "https://www.youtube.com/embed/" + videoID;
-    var videoContainer = document.getElementById("videoContainer");
-    videoContainer.innerHTML = '<iframe width="854" height="480" src="' + videoEmbedURL + '" frameborder="0" allowfullscreen></iframe>';
-    document.getElementById("videoURL").value = 'https://www.youtube.com/watch?v=' + videoID;
+    const videoEmbedURL = `https://www.youtube.com/embed/${videoID}`;
+    const videoContainer = document.getElementById('videoContainer');
+    videoContainer.innerHTML = `<iframe width="854" height="480" src="${videoEmbedURL}" frameborder="0" allowfullscreen></iframe>`;
+    document.getElementById('videoURL').value = `https://www.youtube.com/watch?v=${videoID}`;
   }
 };
