f8aee51605adf23b04c9a3527aaeb7d20e427ad0
Yamato Kataoka <34284486+yamatokataoka@users.noreply.github.com>
2023-04-15 12:52:40 +0900

    Store videoID on URL params

--- script.js
+++ script.js
@@ -20,16 +20,26 @@ function embedVideo() {
   const videoEmbedURL = `https://www.youtube.com/embed/${videoID}`;
   const videoContainer = document.getElementById('videoContainer');
   videoContainer.innerHTML = `<iframe width="854" height="480" src="${videoEmbedURL}" frameborder="0" allowfullscreen></iframe>`;
-  localStorage.setItem('videoID', videoID);
+  const params = new URLSearchParams(window.location.search);
+  params.set('v', videoID);
+  const newURL = `${location.pathname}?${params}`;
+  window.history.pushState({}, '', newURL);
 }
 
 // This function loads the video with the given ID if it has been previously embedded
 window.onload = function () {
-  const videoID = localStorage.getItem('videoID');
+  const params = new URLSearchParams(window.location.search);
+  const videoID = params.get('v');
   if (videoID) {
     const videoEmbedURL = `https://www.youtube.com/embed/${videoID}`;
     const videoContainer = document.getElementById('videoContainer');
     videoContainer.innerHTML = `<iframe width="854" height="480" src="${videoEmbedURL}" frameborder="0" allowfullscreen></iframe>`;
     document.getElementById('videoURL').value = `https://www.youtube.com/watch?v=${videoID}`;
   }
+
+  const videoForm = document.getElementById('videoForm');
+  videoForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    embedVideo();
+  });
 };
